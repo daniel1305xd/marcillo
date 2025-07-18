260373cee8fee5a1b83743d32d889c5f58fc96c8
unknown <dmarcillop@uteq.edu.ec>
2025-07-18 17:26:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/crote/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.crote;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+
+import WebServices.Asynchtask;
+
+public class MainActivity extends AppCompatActivity  implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+
+    }
+}
